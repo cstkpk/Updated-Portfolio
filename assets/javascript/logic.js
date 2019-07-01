d3faa3818f7ed0b2b9a5c358d39f1dc7726f591a
CS <Cara@MBP15in2015.local>
2019-06-30 20:29:10 -0400

    Basics of Contact page finished

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.materialboxed').materialbox();
-  });
+  
+    $('#textarea1').val();
+    M.textareaAutoResize($('#textarea1'));
+
+});
